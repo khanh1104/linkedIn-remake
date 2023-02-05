c4ef17a4f4d74bf28c87f55bcac315aa0c4314bc
khanh1104 <kgp110411@gmail.com>
2023-02-05 08:21:42 +0700

    Adding More jobs & Work sidebar

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// Element alias definition
 var search = document.querySelector('#search');
 var userNav = document.querySelector('#user_nav');
 var userDropdown = document.querySelector('#userDropdown');
 var closeButton = document.querySelector('#close_button');
 var searchSuggestions = document.querySelector('#search_suggestions');
-// Function definitions
+var workDropdown = document.querySelector('#workDropdown_container');
+var workNav = document.querySelector('#work_nav');
+var body = document.querySelector('body');
+var workDropdownCloseButton = document.querySelector('#close_button2');
+var pageBody = document.querySelector('#body');
 function searchboxOutlineOn() {
 	search.style.outline = 'auto';
 }
@@ -17,8 +20,17 @@ function userDropdownShow() {
 function searchSuggestionsHide() {
 	searchSuggestions.style.display = 'none';
 }
-// Event listener
+function workDropdownShow() {
+	workDropdown.style.display = 'block';
+	body.style.overflow = 'hidden';
+}
+function workDropdownHide() {
+	workDropdown.style.display = 'none';
+	body.style.overflow = 'auto';
+}
 search.addEventListener('focusin', searchboxOutlineOn);
 search.addEventListener('focusout', searchboxOutlineOff);
 userNav.addEventListener('click', userDropdownShow);
 closeButton.addEventListener('click', searchSuggestionsHide);
+workNav.addEventListener('click', workDropdownShow);
+workDropdownCloseButton.addEventListener('click', workDropdownHide);
